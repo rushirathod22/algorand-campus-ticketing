11eceff0100050e0a2039f7f829c27bfad3760d3
Rushikesh <rushirathod22082005@gmail.com>
2026-02-06 14:22:01 +0530

    Added frontend logic placeholders

---
+++ frontend/app.js
@@ -0,0 +1,13 @@
+// Frontend logic for Campus Event Ticketing
+
+function connectWallet() {
+  alert("Wallet connection will be implemented here");
+}
+
+function buyTicket() {
+  alert("Ticket purchase logic will be implemented here");
+}
+
+function verifyTicket() {
+  alert("Ticket verification logic will be implemented here");
+}
