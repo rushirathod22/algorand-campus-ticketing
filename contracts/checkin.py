8b4f340aaf2ec104db4457b51e97b035395bb82e
Rushikesh <rushirathod22082005@gmail.com>
2026-02-06 10:19:15 +0530

    Added ticket check-in contract file

---
+++ contracts/checkin.py
@@ -0,0 +1,12 @@
+"""
+On-Chain Ticket Check-In Contract
+
+Purpose:
+- Verify that a wallet owns a valid ticket NFT
+- Mark ticket as used (burn or lock)
+- Prevent duplicate entry using the same ticket
+
+This logic will be implemented using Algorand smart contracts.
+"""
+
+# Check-in logic will be added here
