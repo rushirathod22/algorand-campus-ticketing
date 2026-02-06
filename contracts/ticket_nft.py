9346b0895c19d15d231a06e561f978bdd5fbd405
Rushikesh <rushirathod22082005@gmail.com>
2026-02-06 10:15:23 +0530

    Added ticket NFT smart contract file

---
+++ contracts/ticket_nft.py
@@ -0,0 +1,12 @@
+"""
+Ticket NFT Smart Contract
+
+Purpose:
+- Create NFT tickets for campus events
+- Each NFT represents one valid event ticket
+- Tickets will be non-transferable (soulbound)
+
+This file will be implemented using PyTeal.
+"""
+
+# Smart contract logic will be added here
